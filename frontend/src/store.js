bc37d944273e22c42f28e23af19eab57116ca6a1
anantsb <anantbhagat44@gmail.com>
2023-05-19 11:40:59 +0530

    first commit

---
+++ frontend/src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authslice";
+import { apiSlice } from "./slices/apislice";
+
+const store = configureStore({
+  reducer: { [apiSlice.reducerPath]: apiSlice.reducer, auth: authReducer },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: true,
+});
+
+export default store;
